eb7e85701e37d6fc2da91291abac10c9b096c89d
Génesis Morales <genesismoralesc1@gmail.com>
2024-10-10 10:20:34 -0600

    Revision de codigo 2.0

---
+++ backend/Brickly.LOGIC/Services/Contract/PortfolioTypeService/IPortfolioTypeService.cs
@@ -0,0 +1,7 @@
+﻿using Brickly.DTO;
+
+namespace Brickly.LOGIC.Services.Contract.PortfolioTypeService {
+    public interface IPortfolioTypeService {
+        Task<PortfolioTypeDto> CreatePortfolioTypeAsync(PortfolioTypeDto portfolioTypeDto);
+    }
+}

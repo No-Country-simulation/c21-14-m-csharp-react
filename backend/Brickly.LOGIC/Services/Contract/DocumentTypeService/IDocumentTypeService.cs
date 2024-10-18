f9640c47e7f9fa1ec45af993a1657fd923e1fdc0
Claus <xclauscastillox@gmail.com>
2024-10-18 16:34:59 -0300

    Delete archives from c#

--- backend/Brickly.LOGIC/Services/Contract/DocumentTypeService/IDocumentTypeService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Brickly.DTO;
-
-namespace Brickly.LOGIC.Services.Contract.DocumentTypeService;
-
-public interface IDocumentTypeService
-{
-    Task<DocumentTypeDto> CreateDocumentTypeAsync(DocumentTypeDto documentTypeDto);
-}

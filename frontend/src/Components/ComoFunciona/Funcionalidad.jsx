554efda75913bf405239c56736075bac3004630c
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-10-23 17:17:03 -0400

    Algunos cambios de estilo en el home

--- frontend/src/Components/ComoFunciona/Funcionalidad.jsx
+++ frontend/src/Components/ComoFunciona/Funcionalidad.jsx
@@ -7,7 +7,7 @@ export const Funcionalidad = () => {
     <div className="c_Funcionalidad p-3 ">
         <h2>¿Cómo funciona?</h2>
 
-        <div className='parent'>
+        <div className='parent p-5 gap-5'>
             <div className="div1"><h4>1. Regístrate</h4><p>Crea tu cuenta Brickly totalmente gratis</p></div>
             <div className="div2"><h4>2. Selecciona un proyecto</h4><p>Selecciona la oportunidad de inversión que más se adapte a tus objetivos y la cantidad a invertir.</p></div>
             <div className="div3"><h4>3. Completa tu inversión</h4><p>Finaliza tu inversión firmando el contrato en línea y fondea tu cuenta  Brickly.</p></div>
